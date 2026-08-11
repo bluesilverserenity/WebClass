f65c92ddaac5f8c0450df5ba061ff1dd5b6c4de8
bluesilverserenity <idamunyad@gmail.com>
2026-08-11 14:44:44 +0100

    we have done so far

---
+++ htmlClass/HTMLScript/app.js
@@ -0,0 +1,4 @@
+alert('Hello Javascript')
+
+let h1Col = document.getElementsByTagName('h1')
+h1Col[0].style.color = 'green'
---
+++ htmlClass/HTMLScript/scripts/myApp.js
@@ -0,0 +1,2 @@
+document.writeln('Hello World!');
+document.writeln('<h1> Hello from myApp.js </h1>');
